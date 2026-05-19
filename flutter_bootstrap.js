0d460bf1c5bfc6e8160f12436aae2f4977b3d2dc
Michał Mokrzanowski <michal.mokrzanowski@takeda.com>
2026-05-19 13:12:41 +0200

    Fix logo path in splash screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3903985222" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2606277560" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
